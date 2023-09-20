955b8274c26a062ac92360dfc22180492f7ac9fb
tgoandrex <tgoandrex@gmail.com>
2023-09-19 19:15:15 -0500

    Convert hero-background from webp to jpg because production build would not display background when it was a webp. Reverted index.d.ts in src/assets/images to original. Change backgroundImage in tailwind.config.ts to hero-background.jpg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
     theme: {
         extend: {
           backgroundImage: {
-            "hero-pattern": "url('src/assets/images/hero-background.webp')"
+            "hero-pattern": "url('src/assets/images/hero-background.jpg')"
           }
         }
     },
