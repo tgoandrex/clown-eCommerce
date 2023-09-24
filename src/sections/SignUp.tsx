0836905590b8f6edcd63ee63dc5760ddebcfadcd
tgoandrex <tgoandrex@gmail.com>
2023-09-23 21:09:33 -0500

    Complete Sign Up section

--- src/sections/SignUp.tsx
+++ src/sections/SignUp.tsx
@@ -1,6 +1,14 @@
+import { Button } from "../components";
+
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <section id="sign-up" className="flex max-lg:flex-col w-full max-container justify-between items-center gap-10">
+      <h3 className="heading-three">Sign up for our newsletter!</h3>
+      <div className="w-full lg:max-w-[40%] border border-slate-500 rounded-full flex items-center justify-between gap-5 p-2.5">
+        <input type="text" placeholder="example@email.com" className="h-8 w-[50%] md:w-[60%] lg:w-[55%]" />
+        <Button label="Sign Up"/>
+      </div>
+    </section>
   )
 }
 
